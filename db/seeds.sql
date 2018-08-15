0be3cfa772a7137e0e10c55cc74f405d6221502b
Mystic5523 <Mystic5523@hotmail.com>
2018-08-14 20:09:52 -0500

    db set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesebuger');
+INSERT INTO burgers (burger_name) VALUES ('Cheesesteak Buger');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Cheese Cheesebuger');
