141eefe72d61098de1fc0270a99db1f56753eb52
Mystic5523 <Mystic5523@hotmail.com>
2018-08-17 22:01:14 -0500

    db

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "testtest",
